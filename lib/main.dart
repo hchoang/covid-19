c89a2d40cd184615974169e396af955e1aa673a9
Huy Cat Hoang <cat.hoanghuy@gmail.com>
2020-04-16 23:25:55 +1000

    updated - added dropdown to select country

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:covid19/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,25 +29,110 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              height: 350,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF3383CD),
+                    Color(0xFF11249F)
+                  ]
+                ),
+                image: DecorationImage(
+                  image: AssetImage("assets/images/virus.png")
+                )
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: SvgPicture.asset("assets/icons/menu.svg"),
+                  ),
+                  SizedBox(height: 20,),
+                  Expanded(child: Stack(
+                    children: <Widget>[
+                      SvgPicture.asset(
+                        "assets/icons/Drcorona.svg",
+                        width: 230,
+                        fit: BoxFit.fitWidth,
+                        alignment: Alignment.topCenter,
+                      ),
+                      Positioned(
+                        top: 20,
+                        left: 150,
+                        child: Text('All you need \nis stay at home.', style: kHeadingTextStyle.copyWith(
+                          color: Colors.white
+                        ),),
+                      ),
+                      Container()
+                    ],
+                  ),
+                  )
+                ],
+              ),
+            ),
+          ),
           Container(
-            height: 350,
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            height: 60,
             width: double.infinity,
             decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [
-                  Color(0xFF3383CD),
-                  Color(0xFF11249F)
-                ]
-              ),
-              image: DecorationImage(
-                image: AssetImage()
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: Color(0xFFE5E5E5)
               )
+            ),
+            child: Row(
+              children: <Widget>[
+                SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+                SizedBox(width: 20,),
+                Expanded(
+                  child: DropdownButton(
+                    isExpanded: true,
+                    underline: SizedBox(),
+                    icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                    value: 'Indonesia',
+                    items: ['Indonesia', 'Bangladesh', 'US', 'Japan']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                  }).toList(),
+                    onChanged: (value) {},
+                  ),
+                )
+              ],
             ),
           )
         ],
       ),
     );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(size.width / 2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return false;
   }
 }
